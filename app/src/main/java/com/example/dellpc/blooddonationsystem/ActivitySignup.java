030be491ae97798fd1f90a13d6fbc25db00c8ee3
Ibad Qureshi <ibrehman1995@gmail.com>
2017-02-26 13:59:40 +0500

    add authentication on sign in and setting spinner in signup

--- app/src/main/java/com/example/dellpc/blooddonationsystem/ActivitySignup.java
+++ app/src/main/java/com/example/dellpc/blooddonationsystem/ActivitySignup.java
@@ -2,12 +2,23 @@ package com.example.dellpc.blooddonationsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class ActivitySignup extends AppCompatActivity {
+    EditText EditFirstName;
+    EditText EditLastName;
+    EditText EditEmail;
+    Spinner SelectSpinner;
+    EditText EditPass;
+    Button BtnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
     }
 }
--- app/src/main/java/com/example/dellpc/blooddonationsystem/MainActivity.java
+++ app/src/main/java/com/example/dellpc/blooddonationsystem/MainActivity.java
@@ -2,30 +2,98 @@ package com.example.dellpc.blooddonationsystem;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     EditText EditUserText;
     EditText EditPassText;
     Button BtnLogin;
     TextView TextSignup;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = mFirebaseDatabase.getInstance();
+
+        EditUserText = (EditText) findViewById(R.id.Edit_userName);
+        EditPassText = (EditText) findViewById(R.id.Edit_userPassword);
+        BtnLogin  = (Button) findViewById(R.id.Btn_login);
         TextSignup = (TextView) findViewById(R.id.TextSignup);
-        TextSignup.setOnClickListener(new View.OnClickListener() {
+
+
+        //attaching a authentication listener
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivitySignup.class);
-                startActivity(intent);
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
             }
-        });
+        };
+    }
+    public void Signin(View view){
+        //sign in with existing user
+        mFirebaseAuth.signInWithEmailAndPassword(EditUserText.getText().toString(),EditPassText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(MainActivity.this,"User not exists",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+    public void Signup(View view){
+        Intent intent = new Intent(MainActivity.this, ActivitySignup.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mFirebaseAuth.removeAuthStateListener(mAuthListener);
     }
 }
