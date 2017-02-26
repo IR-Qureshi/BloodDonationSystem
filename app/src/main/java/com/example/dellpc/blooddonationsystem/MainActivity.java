8d8c948f2149bfca8c426edc51004063427eb8e9
Ibad Qureshi <ibrehman1995@gmail.com>
2017-02-26 11:58:50 +0500

    setting up a signIn/ signUp UI

--- app/src/main/java/com/example/dellpc/blooddonationsystem/MainActivity.java
+++ app/src/main/java/com/example/dellpc/blooddonationsystem/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.dellpc.blooddonationsystem;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText EditUserText;
+    EditText EditPassText;
+    Button BtnLogin;
+    TextView TextSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextSignup = (TextView) findViewById(R.id.TextSignup);
+        TextSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivitySignup.class);
+                startActivity(intent);
+            }
+        });
     }
 }
